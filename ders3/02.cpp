98ad02100b8096f79c208f68bbfc1c157bcf78e3
burhansert <burhansert50@gmail.com>
2023-06-14 18:04:36 +0300

    04

---
+++ ders3/02.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
+cars[0] = "Opel";
+cout << cars[0]<<endl;
+
+system("PAUSE"); //program kapanmasin
+
+return 0;
+
+}
