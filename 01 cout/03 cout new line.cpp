703a5f18edf90bbd8d14bffdffd55561bc4581ed
burhansert <burhansert50@gmail.com>
2023-12-20 22:00:37 +0300

    .

---
+++ 01 cout/03 cout new line.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World1" << endl;
+  cout << "Hello World2 \n\n";
+  cout << "I am learning C++";
+  return 0;
+}
---
+++ 02 Comments/01 Comments.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  /* The code below will print the words Hello World!
+  to the screen, and it is amazing */
+  cout << "Hello World!";
+  //merhaba
+  return 0;
+}
